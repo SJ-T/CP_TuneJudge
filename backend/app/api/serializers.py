13735b6da6d505b086638d1cc3013914463487c5
Sijia <sijia.tang@code.berlin>
2024-10-14 02:23:07 +0200

    [ADD] backend api to get a random music clip

--- backend/app/api/serializers.py
+++ backend/app/api/serializers.py
@@ -5,7 +5,15 @@ from ..models import Music, Rating
 class MusicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Music
-        fields = ['id', 'title', 'label', 'file']
+        fields = [
+            'id', 'title', 'label', 'file', 'key',
+            'npvi', 'note_density', 'pitch_range',
+            'pitch_count', 'pitch_class_count',
+            'pitch_entropy', 'pitch_class_entropy',
+            'pitch_in_scale_rate', 'scale_consistency',
+            'polyphony', 'polyphony_rate',
+            'complexity', 'originality', 'gradus'
+        ]
 
 
 class RatingSerializer(serializers.ModelSerializer):
--- backend/app/api/views.py
+++ backend/app/api/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from random import choice
 from ..models import Music, Rating
 from .serializers import MusicSerializer, RatingSerializer
 
@@ -6,6 +9,16 @@ from .serializers import MusicSerializer, RatingSerializer
 class MusicViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Music.objects.all()
     serializer_class = MusicSerializer
+
+    @action(detail=False, methods=['get'])
+    def random(self, request):
+        labels = Music.objects.values_list('label', flat=True).distinct()
+        random_label = choice(labels)
+        random_track = Music.objects.filter(label=random_label).order_by('?').first()
+        if random_track:
+            serializer = self.get_serializer(random_track)
+            return Response(serializer.data)
+        return Response({"error": "No music tracks available"}, status=404)
 
 
 class RatingViewSet(viewsets.ModelViewSet):
