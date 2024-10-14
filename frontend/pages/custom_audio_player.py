0ec0cd79070c0341769b6ca25422e1b6dcc552e5
Sijia <sijia.tang@code.berlin>
2024-10-14 02:23:59 +0200

    [ADD] design for music player

---
+++ frontend/pages/custom_audio_player.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+
+def custom_audio_player(audio_url, css_path):
+    with open(css_path, 'r') as f:
+        css = f'<style>{f.read()}</style>'
+    custom_html = f"""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+        {css}
+    </head>
+    <body>
+        <div class="audio-container">
+            <audio id="audioPlayer" src="{audio_url}"></audio>
+            <button id="playPauseBtn" class="play-button">Play</button>
+        </div>
+
+        <script>
+            var audio = document.getElementById('audioPlayer');
+            var playPauseBtn = document.getElementById('playPauseBtn');
+
+            playPauseBtn.addEventListener('click', function() {{
+                if (audio.paused) {{
+                    audio.play();
+                    playPauseBtn.textContent = 'Pause';
+                }} else {{
+                    audio.pause();
+                    playPauseBtn.textContent = 'Play';
+                }}
+            }});
+        </script>
+    </body>
+    </html>
+    """
+    components.html(custom_html, height=0)
--- frontend/pages/turing_test.py
+++ frontend/pages/turing_test.py
@@ -1,19 +1,25 @@
 import streamlit as st
 import requests
+import os
+from custom_audio_player import custom_audio_player
+
 
 API_BASE_URL = 'http://localhost:8000/api/'
 
+st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
+def load_css(file_path):
+    with open(file_path) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-def fetch_data(end_point):
-    response = requests.get(end_point)
+
+def fetch_random_music():
+    response = requests.get(API_BASE_URL + 'music/random/')
     response.raise_for_status()
     return response.json()
 
+random_track = fetch_random_music()
 
-music_data = fetch_data(API_BASE_URL + 'music')
+if random_track:
+    custom_audio_player(random_track['file'], 'frontend/style.css')
 
-# st.write(music_data)
-for track in music_data:
-    st.write(track['title'])
-    st.audio(track['file'])
-
+load_css('frontend/style.css')
