76fbc11389a16088d0c7d903822c2135fc36df05
Sijia <sijia.tang@code.berlin>
2024-10-21 00:14:19 +0200

    [ADJ] remove comment

--- frontend/Home.py
+++ frontend/Home.py
@@ -4,7 +4,6 @@ import sys
 from pathlib import Path
 from utils import load_css
 
-# Add the project root to the Python path
 project_root = Path(__file__).resolve().parent.parent
 sys.path.append(str(project_root))
 
